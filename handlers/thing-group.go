05e4e693a5b4b8e0553dfbf37f3c079a58364ccd
Luc Chmielowski <luc.chmielowski@gmail.com>
2017-06-13 01:18:45 +0200

    Updated gateway

--- handlers/thing-group.go
+++ handlers/thing-group.go
@@ -2,12 +2,11 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/iochti/gateway-service/helpers"
+	"github.com/iochti/thing-group-service/models"
 	pb "github.com/iochti/thing-group-service/proto"
 )
 
@@ -22,66 +21,56 @@ func (t *ThingGroupHandler) HandleGetGroup(w http.ResponseWriter, r *http.Reques
 	w.Header().Set("Content-Type", t.ContentType)
 	ctx := helpers.GetContext(r)
 	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	rsp, err := t.ThingGroupSvc.GetGroup(ctx, &pb.GroupIDRequest{ID: int32(id)})
+	rsp, err := t.ThingGroupSvc.GetGroup(ctx, &pb.GroupIDRequest{ID: vars["id"]})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	byteResp, err := json.Marshal(rsp)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	w.Write(byteResp)
+	w.Write(rsp.GetItem())
 }
 
 // HandleCreateGroup handles POST#/group and sends the created group as JSON
 func (t *ThingGroupHandler) HandleCreateGroup(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", t.ContentType)
 	ctx := helpers.GetContext(r)
-	group := new(pb.ThingGroup)
+	group := new(models.ThingGroup)
 	if err := json.NewDecoder(r.Body).Decode(group); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	fmt.Println(group)
-	rsp, err := t.ThingGroupSvc.CreateGroup(ctx, group)
+	bytes, err := json.Marshal(group)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	bytesResp, err := json.Marshal(rsp)
+	rsp, err := t.ThingGroupSvc.CreateGroup(ctx, &pb.ThingGroup{Item: bytes})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Write(bytesResp)
+	w.Write(rsp.GetItem())
 }
 
 // HandleUpdateGroup handles PUT#/group and sends the updated group as JSON
 func (t *ThingGroupHandler) HandleUpdateGroup(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", t.ContentType)
 	ctx := helpers.GetContext(r)
-	group := new(pb.ThingGroup)
-	if err := json.NewDecoder(r.Body).Decode(group); err != nil {
+	group := models.ThingGroup{}
+	if err := json.NewDecoder(r.Body).Decode(&group); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	rsp, err := t.ThingGroupSvc.UpdateGroup(ctx, group)
+	bytes, err := json.Marshal(group)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	rsp, err := t.ThingGroupSvc.UpdateGroup(ctx, &pb.ThingGroup{Item: bytes})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	bytesResp, err := json.Marshal(rsp)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Write(bytesResp)
+	w.Write(rsp.GetItem())
 }
 
 // HandleDeleteGroup handles DELETE#/group and sends the deleted id as JSON
@@ -89,18 +78,14 @@ func (t *ThingGroupHandler) HandleDeleteGroup(w http.ResponseWriter, r *http.Req
 	w.Header().Set("Content-Type", t.ContentType)
 	ctx := helpers.GetContext(r)
 	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-	}
-	_, err = t.ThingGroupSvc.DeleteGroup(ctx, &pb.GroupIDRequest{ID: int32(id)})
+	_, err := t.ThingGroupSvc.DeleteGroup(ctx, &pb.GroupIDRequest{ID: vars["id"]})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	bytesResp, err := json.Marshal(struct {
-		ID int `json:"id_deleted"`
-	}{id})
+		ID string `json:"id_deleted"`
+	}{vars["id"]})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
---
+++ vendor/github.com/iochti/thing-group-service/models/thing-group.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+// ThingGroup represents a group of thing
+type ThingGroup struct {
+	ID          bson.ObjectId `json:"id" bson:"_id"`
+	AccountID   string        `json:"account_id" bson:"accountId"`
+	Name        string        `json:"name" bson:"name"`
+	Description string        `json:"description" bson:"description"`
+	CreatedAt   time.Time     `json:"created_at" bson:"createdAt"`
+	UpdatedAt   time.Time     `json:"updated_at" bson:"updatedAt"`
+}
